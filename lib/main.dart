45689764f608404a4505f6f136652932d7cd9b1c
Prathik612 <prathik612@gmail.com>
2022-12-29 11:48:18 +0300

    update 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:math_expressions/math_expressions.dart';
 import 'package:intl/intl.dart';
 
 void main() {
@@ -36,6 +33,7 @@ class _CalculatorState extends State<Calculator> {
   String expr = '';
   String curr = '0';
   int flag = 0;
+  int flag1 = 0;
   double val = 0.0;
   final List<String> buttons = [
     'AC',
@@ -62,170 +60,192 @@ class _CalculatorState extends State<Calculator> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 63, 3, 33),
-      body: Column(
-        children: <Widget>[
-          const SizedBox(
-            height: 29,
-          ),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 122, 7, 66),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10.0),
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      expr,
-                      style: const TextStyle(
-                        fontSize: 25,
-                        color: Colors.grey,
+      backgroundColor: Colors.black87,
+      body: SafeArea(
+        minimum: const EdgeInsets.all(6.0),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                alignment: Alignment.bottomRight,
+                decoration: BoxDecoration(
+                  color: Colors.greenAccent[400],
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10.0),
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          expr,
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.grey[700],
+                          ),
+                        ),
                       ),
-                    ),
+                      Container(
+                        padding: const EdgeInsets.all(10.0),
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          curr,
+                          style: const TextStyle(
+                            fontSize: 47,
+                            color: Colors.black,
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                  Container(
-                    padding: const EdgeInsets.all(10.0),
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      curr,
-                      style: const TextStyle(
-                        fontSize: 47,
-                        color: Colors.white,
-                      ),
-                    ),
-                  )
-                ],
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Expanded(
-            flex: 2,
-            child: GridView.builder(
-              // padding: const EdgeInsets.all(5.0),
-              itemCount: buttons.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                //AC bttn
-                if (index == 0) {
-                  return Bttn(
-                    ontap: () {
-                      setState(() {
-                        expr = '';
-                        curr = '0';
-                      });
-                    },
-                    bttnTxt: buttons[index],
-                    color: Colors.amber[800],
-                    txtColor: Colors.black,
-                  );
-                }
-                //+/- bttn
-                else if (index == 1) {
-                  return Bttn(
-                    ontap: () {
-                      setState(() {
-                        if (curr[0] == '-') {
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              flex: 2,
+              child: GridView.builder(
+                // padding: const EdgeInsets.all(5.0),
+                itemCount: buttons.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  //AC bttn
+                  if (index == 0) {
+                    return Bttn(
+                      ontap: () {
+                        setState(() {
+                          expr = '';
+                          curr = '0';
+                        });
+                      },
+                      bttnTxt: buttons[index],
+                      color: Colors.red[800],
+                      txtColor: Colors.black,
+                    );
+                  }
+                  //+/- bttn
+                  else if (index == 1) {
+                    return Bttn(
+                      ontap: () {
+                        setState(() {
+                          if (curr[0] == '-') {
+                            expr = expr.substring(0, expr.length - curr.length);
+                            curr = curr.substring(1);
+                            expr += curr;
+                          } else {
+                            expr = expr.substring(0, expr.length - curr.length);
+                            curr = '-$curr';
+                            expr += curr;
+                          }
+                        });
+                      },
+                      bttnTxt: buttons[index],
+                      color: Colors.grey[700],
+                      txtColor: Colors.black,
+                    );
+                  }
+                  //% bttn
+                  else if (index == 2) {
+                    return Bttn(
+                      ontap: () {
+                        setState(() {
+                          val = double.parse(curr);
                           expr = expr.substring(0, expr.length - curr.length);
-                          curr = curr.substring(1);
+                          val = val / 100;
+                          curr = val.toString();
                           expr += curr;
-                        } else {
-                          expr = expr.substring(0, expr.length - curr.length);
-                          curr = '-$curr';
-                          expr += curr;
-                        }
-                      });
-                    },
-                    bttnTxt: buttons[index],
-                    color: Colors.pink[400],
-                    txtColor: Colors.black,
-                  );
-                }
-                //% bttn
-                else if (index == 2) {
-                  return Bttn(
-                    ontap: () {
-                      setState(() {
-                        val = double.parse(curr);
-                        expr = expr.substring(0, expr.length - curr.length);
-                        val = val / 100;
-                        curr = val.toString();
-                        expr += curr;
-                      });
-                    },
-                    bttnTxt: buttons[index],
-                    color: Colors.pink[400],
-                    txtColor: Colors.black,
-                  );
-                }
-                //DEL bttn
-                else if (index == 18) {
-                  return Bttn(
-                    ontap: () {
-                      setState(() {
-                        if (curr == '') {
-                        } else {
-                          expr = expr.substring(0, expr.length - 1);
-                          curr = curr.substring(0, curr.length - 1);
-                        }
-                      });
-                    },
-                    bttnTxt: buttons[index],
-                    color: Colors.pink[900],
-                    txtColor: Colors.black,
-                  );
-                }
-                //equal bttn
-                else if (index == 19) {
-                  return Bttn(
-                    ontap: () {
-                      setState(() {
-                        evaluate();
-                        expr=curr;
-                      });
-                    },
-                    bttnTxt: buttons[index],
-                    color: Colors.pink[400],
-                    txtColor: Colors.black,
-                  );
-                } else {
-                  return Bttn(
-                    ontap: () {
-                      setState(() {
-                        expr += buttons[index];
-                        if (isOpr(buttons[index])) {
-                          flag = 1;
-                        } else {
-                          if (curr == '0' || flag == 1) {
-                            curr = '';
+                        });
+                      },
+                      bttnTxt: buttons[index],
+                      color: Colors.grey[700],
+                      txtColor: Colors.black,
+                    );
+                  }
+                  //DEL bttn
+                  else if (index == 18) {
+                    return Bttn(
+                      ontap: () {
+                        setState(() {
+                          if (curr == '') {
+                          } else {
+                            expr = expr.substring(0, expr.length - 1);
+                            curr = curr.substring(0, curr.length - 1);
+                          }
+                        });
+                      },
+                      bttnTxt: buttons[index],
+                      color: Colors.grey[400],
+                      txtColor: Colors.black,
+                    );
+                  }
+                  //equal bttn
+                  else if (index == 19) {
+                    return Bttn(
+                      ontap: () {
+                        setState(() {
+                          evaluate(expr);
+                          expr = curr;
+                          flag1 = 0;
+                        });
+                      },
+                      bttnTxt: buttons[index],
+                      color: Colors.grey[700],
+                      txtColor: Colors.black,
+                    );
+                  } else if (index == 17) {
+                    return Bttn(
+                      ontap: () {
+                        setState(() {
+                          if (flag1 == 0) {
+                            expr += buttons[index];
+                            curr += buttons[index];
+                            flag1 = 1;
+                          }
+                        });
+                      },
+                      bttnTxt: buttons[index],
+                      color: Colors.grey[400],
+                      txtColor: Colors.black,
+                    );
+                  } else {
+                    return Bttn(
+                      ontap: () {
+                        setState(() {
+                          if (isOpr(buttons[index]) && flag == 0) {
+                            expr += buttons[index];
+                            flag = 1;
+                            flag1 = 0;
+                          } else if (!(isOpr(buttons[index]))) {
+                            expr += buttons[index];
+                            if (curr == '0' || flag == 1) {
+                              curr = '';
+                              flag = 0;
+                            }
+                            curr += buttons[index];
                             flag = 0;
                           }
-                          curr += buttons[index];
-                        }
-                      });
-                    },
-                    bttnTxt: buttons[index],
-                    color: isOpr(buttons[index])
-                        ? Colors.pink[400]
-                        : Colors.pink[900],
-                    txtColor: Colors.black,
-                  );
-                }
-              },
-            ),
-          )
-        ],
+                        });
+                      },
+                      bttnTxt: buttons[index],
+                      color: isOpr(buttons[index])
+                          ? Colors.grey[700]
+                          : Colors.grey[400],
+                      txtColor: Colors.black,
+                    );
+                  }
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -237,19 +257,45 @@ class _CalculatorState extends State<Calculator> {
     return false;
   }
 
-  void evaluate() {
-    String inp = expr;
-    inp = expr.replaceAll('x', '*').replaceAll('÷', '/');
-    final f = NumberFormat();
+  void evaluate(String e) {
+    double val = 0;
+    double temp1 = 0;
+    String arg1 = '', arg2 = '', opr = '';
+    e = e.replaceAll('x', '*').replaceAll('÷', '/');
 
-    Parser p = Parser();
-    ContextModel cm = ContextModel();
-    Expression exp = p.parse(inp);
-    double ans = exp.evaluate(EvaluationType.REAL, cm);
+    int j = 0;
+    while (RegExp(r'^[0-9]+$').hasMatch(e[j]) || e[j] == '.') {
+      arg1 += e[j];
+      j++;
+    }
+    val = double.parse(arg1);
+
+    for (int i = j; i < e.length;) {
+      opr = e[i];
+      i++;
+      while (
+          i < e.length && (RegExp(r'^[0-9]+$').hasMatch(e[i]) || e[i] == '.')) {
+        arg2 += e[i];
+        i++;
+      }
+
+      temp1 = double.parse(arg2);
+
+      if (opr == '+') {
+        val += temp1;
+      } else if (opr == '-') {
+        val -= temp1;
+      } else if (opr == '*') {
+        val *= temp1;
+      } else if (opr == '/') {
+        val /= temp1;
+      }
+      arg2 = '';
+    }
+    final f = NumberFormat();
     f.minimumFractionDigits = 0;
     f.maximumFractionDigits = 10;
-    // curr = ans.toString();
-    curr = f.format(ans);
+    curr = f.format(val);
   }
 }
 
